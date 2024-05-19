f76234fced8166b39a23859d8ea2cc5b6bcc4380
m1ssy0u <varakorn2324@gmail.com>
2024-05-19 23:57:08 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Profile page of Varakorn",
+  title: "Profile",
   description: "created by my self",
 };
 
