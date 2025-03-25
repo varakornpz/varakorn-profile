92a7d83a43f88c42012e1132e0bbc3f7ae0fe621
Varakorn Kaewmanee <varakorn2324@gmail.com>
2025-03-25 20:01:33 +0700

    ahhhh add OG image tsx

---
+++ src/app/opengraph-image.tsx
@@ -0,0 +1,12 @@
+
+import OGImage from './opengraph-image.png'
+ 
+export const alt = 'varakorn.net'
+ 
+export const contentType = 'image/png'
+ 
+
+export default  function Image() {
+
+  return OGImage
+}
