f66ab0e5159dbd67fc368a8eae193d625f530904
Varakorn Kaewmanee <varakorn2324@gmail.com>
2025-03-26 21:54:53 +0700

    Update BasicInfo.tsx

--- src/app/about/BasicInfo.tsx
+++ src/app/about/BasicInfo.tsx
@@ -5,7 +5,7 @@ export default ()=>{
             <div>
                 <div className="flex flex-col items-center">
                     <h1 className="text-white font-semibold sm:text-2xl sm:mt-[15%] mt-[60%] text-center">
-                        This Page is Loading... Slowly... Maybe Forever? 🌀
+                        This Page is Loading... Slowly... Maybe Forever? <p className="sm:inline hidden">🌀</p>
                     </h1>
                     <h2 className="text-white sm:text-xl  mt-7 text-center">
                         Something is being made here. Will it be good? Who knows! But hey, at least I'm trying.
