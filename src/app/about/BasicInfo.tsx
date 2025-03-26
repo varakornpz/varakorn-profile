f1fe303d74b90736c9ef27ac2b4727284e51dd0f
Varakorn Kaewmanee <varakorn2324@gmail.com>
2025-03-26 21:51:30 +0700

    add under develop page

--- src/app/about/BasicInfo.tsx
+++ src/app/about/BasicInfo.tsx
@@ -2,10 +2,15 @@ export default ()=>{
 
 
     return(
-        <div className="text-white md:pt-16">
             <div>
-                
+                <div className="flex flex-col items-center">
+                    <h1 className="text-white font-semibold sm:text-2xl sm:mt-[15%] mt-[60%] text-center">
+                        This Page is Loading... Slowly... Maybe Forever? 🌀
+                    </h1>
+                    <h2 className="text-white sm:text-xl  mt-7 text-center">
+                        Something is being made here. Will it be good? Who knows! But hey, at least I'm trying.
+                    </h2>
+                </div>
             </div>
-        </div>
     )
 }
