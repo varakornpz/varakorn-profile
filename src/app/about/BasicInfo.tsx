3384154162bac7239657c9b713902a5019fdf065
Varakorn Kaewmanee <varakorn2324@gmail.com>
2025-03-26 22:02:56 +0700

    Update BasicInfo.tsx

--- src/app/about/BasicInfo.tsx
+++ src/app/about/BasicInfo.tsx
@@ -5,10 +5,10 @@ export default ()=>{
             <div>
                 <div className="flex flex-col items-center">
                     <h1 className="text-white font-semibold sm:text-2xl sm:mt-[15%] mt-[60%] text-center">
-                        This Page is Loading... Slowly... Maybe Forever? <p className="sm:inline hidden">🌀</p>
+                        Under Construction... Maybe?<p className="sm:inline hidden">🤔</p>
                     </h1>
                     <h2 className="text-white sm:text-xl  mt-7 text-center">
-                        Something is being made here. Will it be good? Who knows! But hey, at least I'm trying.
+                        We're working on something here... not sure if it's genius or a total disaster. Stay tuned!
                     </h2>
                 </div>
             </div>
